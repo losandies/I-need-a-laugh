d3985a485c73da0c697f5f7b3f719d2e3131263c
Brandon Newsome <brandon.i.newsome@gmail.com>
2022-03-26 01:34:14 -0400

    added 2 joke limit/refactored code

--- script.js
+++ script.js
@@ -7,62 +7,69 @@ let fromMe2 = document.createElement('p');
 let typing = document.createElement('p');
 let emojis = document.querySelectorAll('.emoji');
 
-fromThem.classList.add('from-them');
-fromThem2.classList.add('from-them');
+let api_key = API_KEY;
+
+fromThem.className = 'from-them';
+fromThem2.className = 'from-them';
 fromMe.className = 'from-me no-tail emoji';
-fromMe2.classList.add('from-me');
+fromMe2.className = 'from-me';
 typing.className = 'from-them typing';
 
-let generateJoke = async () => {
-	const jokeRes = await fetch('https://dad-jokes.p.rapidapi.com/random/joke', {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'x-rapidapi-host': 'dad-jokes.p.rapidapi.com/',
-			'x-rapidapi-key': '49ed49f35emshd0c7931fd78d141p1e2c99jsnd1494b2bb065',
-		},
-	});
-
-	const joke = await jokeRes.json();
-	const setup = joke.body[0].setup;
-	const punchline = joke.body[0].punchline;
-
-	const dialogue = () => {
-		iMessage.appendChild(fromThem);
-		// fromThem.innerText = setup;
-		fromThem.innerText = setup;
-
-		setTimeout(() => {
-			iMessage.appendChild(typing);
-			typing.innerText = 'Typing...';
-		}, 1000);
-
-		setTimeout(() => {
-			iMessage.removeChild(typing);
-			iMessage.appendChild(fromThem2);
-			fromThem2.innerText = punchline;
-		}, 4000);
-
-		emojis.forEach((emoji) => {
-			emoji.addEventListener('click', (e) => {
-				e.preventDefault();
-				iMessage.appendChild(fromMe);
-				fromMe.innerText = e.target.innerText;
-			});
-		});
-	};
-
-	dialogue();
-};
+// Keeps iMessage container scrolled down as messages flow
+iMessage.scrollTop = iMessage.scrollHeight - iMessage.clientHeight;
 
 let count = 0;
 
-if (count <= 2) {
-	jokeButton.addEventListener('click', generateJoke);
+let initiateChat = async () => {
+	if (count <= 2) {
+		const jokeRes = await fetch(
+			'https://dad-jokes.p.rapidapi.com/random/joke',
+			{
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'x-rapidapi-host': 'dad-jokes.p.rapidapi.com/',
+					'x-rapidapi-key': `${api_key}`,
+				},
+			}
+		);
+
+		const joke = await jokeRes.json();
+		const setup = joke.body[0].setup;
+		const punchline = joke.body[0].punchline;
+
+		const pushDialogue = () => {
+			iMessage.appendChild(fromThem);
+			fromThem.innerText = setup;
+
+			setTimeout(() => {
+				iMessage.appendChild(typing);
+				typing.innerText = 'Typing...';
+			}, 1000);
+
+			setTimeout(() => {
+				iMessage.removeChild(typing);
+				iMessage.appendChild(fromThem2);
+				fromThem2.innerText = punchline;
+			}, 4000);
+
+			emojis.forEach((emoji) => {
+				emoji.addEventListener('click', (e) => {
+					iMessage.appendChild(fromMe);
+					fromMe.innerText = e.target.innerText;
+				});
+			});
+		};
+
+		pushDialogue();
+	} else {
+		alert(
+			"Sorry, I'm poor and cant afford the unlimited jokes API access. Please check back in 24 hours!"
+		);
+	}
+};
+
+jokeButton.addEventListener('click', () => {
 	count++;
-	console.log(count);
-} else {
-	alert(
-		'Sorry, Im poor and cant afford the unlimited jokes API access. check back in 24 hours!'
-	);
-}
+	initiateChat();
+});
